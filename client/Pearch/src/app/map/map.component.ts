50a32e3336c0c8be332ec24196c5d0365c17d588
N-cunningham <n.cunningham12@nuigalway.ie>
2020-11-27 00:26:04 +0000

    multiple constituencies working on map

--- client/Pearch/src/app/map/map.component.ts
+++ client/Pearch/src/app/map/map.component.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { ColourPickerComponent } from '../colour-picker/colour-picker.component';
 import { ColourSwapServiceService } from '../service/colour-swap-service.service';
+import { Place } from '../model/place'
 
 @Component({
   selector: 'app-map',
@@ -11,14 +12,24 @@ import { ColourSwapServiceService } from '../service/colour-swap-service.service
 export class MapComponent implements OnInit {
 
   constructor(private colourSwapServiceService: ColourSwapServiceService) { }
+  colours: Place[] = [{name: "Sligo", colour: "null"}, {name:"Donegal", colour: "null"},{name:"Mayo", colour: "null"}, {name:"Cavan", colour: "null"}, {name:"Roscommon", colour: "null"}];
 
   ngOnInit(): void {
   }
 
   logEvent(constituency: String){
+
     console.log(constituency)
-    let styleText = "--" + constituency + ": " + this.colourSwapServiceService.colour +";";
-    document.querySelector("body").style.cssText = styleText;
+    let styleTexts = ""
+    
+    this.colours.forEach(element => {
+      if (element.name === constituency){
+        element.colour = this.colourSwapServiceService.colour;
+      }
+      styleTexts = styleTexts + "--" + element.name + ": " + element.colour +"; ";
+    });
+    
+    document.querySelector("body").style.cssText = styleTexts;
   }
 
 }
